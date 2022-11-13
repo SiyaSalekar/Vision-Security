4606f23cbc59d31b6b648585b7692a4eeac11a3b
SiyaSalekar <92158161+SiyaSalekar@users.noreply.github.com>
2022-11-13 20:18:08 +0000

    started working on PubNub push QR data scanned to a channel :construction_worker:

--- static/main.js
+++ static/main.js
@@ -10,8 +10,6 @@ function qrcodeFunc(){
             document.getElementById("data_id").innerHTML = "Data Found";
       })
 
-
-
 //    .then(response=>{
 //        if(response.ok){
 //            if(responseStr){
@@ -25,4 +23,46 @@ function qrcodeFunc(){
 //        }
 //        throw new Error("Server offline")
 //    })
+}
+
+//Pubnub
+let myChannel = "siyas-channel"
+let pubnub;
+
+const setupPubNub = ()=>{
+	pubnub = new PubNub({
+		publishKey:
+			"pub-c-7b7bf3ef-a5df-4ec8-9bb2-f70cc90f6c86",
+		subscribeKey:
+			"sub-c-babca055-8ae8-4cbb-87e3-d1927bc7826a",
+		userID:"siyas-windows-machine"
+	});
+	const listener ={
+		status:(statusEvent)=>{
+			if(statusEvent.category === "PNConnectedCategory"){
+				console.log("Connected")
+			}
+		},
+		message:(messageEvent)=>{
+			showMessage(messageEvent.message.description);
+		},
+		presence:(presenceEvent)=>{
+
+		}
+	}
+	pubnub.addListener(listener);
+	//subscribe to a channel
+	pubnub.subscribe({channels:[myChannel]
+	});
+};
+
+const publishMessage = async(message) =>{
+	const publishPayload = {
+		channel:myChannel,
+		message:{
+			title:"Sensor Data",
+			description:message
+		}
+	};
+	await pubnub.publish(publishPayload);
 }
