b98622781935c454d79446d32950c8b467f4883f
ovidiujordache <ovi_jordache@protonmail.com>
2022-11-21 22:15:04 +0000

    adding ultra sound interface to html

--- static/main.js
+++ static/main.js
@@ -5,6 +5,8 @@
 ========== Start Siya's Code for QR Scan==============
 */
 
+
+
 function qrcodeFunc(){
     fetch("/qrgenerate")
       .then(response=>{
@@ -76,56 +78,6 @@ const publishMessage = async(message) =>{
 */ 
 
 
-/*
-Ovidiu 's Code from here on....'
-*/
-
-//snap=document.getElementById('snap-button');
-//snap.addEventListener('click',snapping );
-//terminal=document.getElementById("terminal");
-/*
-confirm=document.getElementById('confirm-snap')
-yes=document.getElementById('yes-button')
-no=document.getElementById('no-button')
-
-yes.addEventListener('click', ()=>{snap.style.display='block';toggle(this)})
-
-no.addEventListener('click',()=>{toggle()} )
-
- let toggle = button=>{
-  
-    let hidden = confirm.getAttribute("hidden");
-
-    if (hidden) {
-       confirm.removeAttribute("hidden");
-      
-     
-    } else {
-       confirm.setAttribute("hidden", "hidden");
-       
-    }
-  }
-  */
-//function snapping(){
-//
-//      //toggle(this)
-//      terminal.innerHTML="I'm sending your mug to  Awesa....\n All good dude/dudess :) !"
-//      setTimeout(function(){terminal.innerHTML=''}, 4000)
-//
-//
-//
-//
-//
-//
-//
-//}
-
-/*
-Ovidiu's code ending here.
-*/
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 
 Annas 's Code from here on....'
---
+++ static/main_ovi.js
@@ -0,0 +1,52 @@
+
+/*
+Ovidiu 's Code from here on....'
+*/
+
+snap=document.getElementById('snap-button');
+snap.addEventListener('click',snapping );
+terminal=document.getElementById("terminal");
+/*
+confirm=document.getElementById('confirm-snap')
+yes=document.getElementById('yes-button')
+no=document.getElementById('no-button')
+
+yes.addEventListener('click', ()=>{snap.style.display='block';toggle(this)})
+
+no.addEventListener('click',()=>{toggle()} )
+
+ let toggle = button=>{
+  
+    let hidden = confirm.getAttribute("hidden");
+
+    if (hidden) {
+       confirm.removeAttribute("hidden");
+      
+     
+    } else {
+       confirm.setAttribute("hidden", "hidden");
+       
+    }
+  }
+  */
+function snapping(){
+
+     //toggle(this)
+     terminal.innerHTML="I'm sending your mug to  Awesa....\n All good dude/dudess :) !"
+     setTimeout(function(){terminal.innerHTML=''}, 4000)
+  }
+//
+//
+//
+//
+//
+//
+//
+//}
+
+/*
+Ovidiu's code ending here.
+*/
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
