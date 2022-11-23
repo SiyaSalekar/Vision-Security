29ddbf8b340496aa340c561dc868d4904fc75687
SiyaSalekar <92158161+SiyaSalekar@users.noreply.github.com>
2022-11-23 08:50:36 +0000

    updated qr-code-sensors.py :label:

--- qr-code-sensors.py
+++ qr-code-sensors.py
@@ -21,7 +21,7 @@ data = {}
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = "sub-c-babca055-8ae8-4cbb-87e3-d1927bc7826a"
 pnconfig.publish_key = "pub-c-7b7bf3ef-a5df-4ec8-9bb2-f70cc90f6c86"
-pnconfig.user_id = "siyas-machine"
+pnconfig.user_id = "Jack-device"
 pubnub = PubNub(pnconfig)
 
 app = Flask(__name__)
@@ -34,7 +34,6 @@ mydb = mysql.connector.connect(
     database='vision_security'
 )
 
-
 # database connect endRegion
 
 
@@ -46,14 +45,18 @@ def index():
         return render_template("greet.html", name=request.form.get("name", "World"))
 
 
-@app.route("/register", methods=["POST"])
+@app.route("/register",methods=["POST"])
 def register():
+
     student_id = request.form.get("student_id")
-    name = request.form.get("student_name")
+    email = request.form.get("student_email")
+    end_date = request.form.get("end_date")
     if not student_id:
         return render_template("error.html", message="Invalid ID")
-    if not name:
-        return render_template("error.html", message="Enter Name")
+    if not email:
+        return render_template("error.html", message="Invalid Email")
+    if not end_date:
+        return render_template("error.html", message="Invalid End Date")
 
     # convert passwd to bytes
     passwd = request.form.get("password").encode()
@@ -68,76 +71,12 @@ def register():
     qr.add_data(password_store)
     qr.make(fit=True)
     img = qr.make_image(fill_color='black', back_color='white')
-    img.save(f"static/images/{name}.png")
-
+    img.save(f"static/images/{student_id}.png")
     mycursor = mydb.cursor()
-    mycursor.execute("insert into student(name, student_id, password) values (%s, %s, %s) ",
-                     (name, student_id, password_store))
+    mycursor.execute("insert into student(student_number, student_email, student_password, course_end_date) values (%s, %s, %s, %s) ", (student_id, email, password_store, end_date))
     mydb.commit()
     mycursor.close()
     return redirect("/")
-
-
-@app.route("/qrgenerate")
-def qrgenerate():
-    dataD["alarm"] = False
-    trigger = False
-    # set up camera object
-    cap = cv2.VideoCapture(-1)
-
-    # QR code detection object
-    detector = cv2.QRCodeDetector()
-    while True:
-        # get the image
-        _, img = cap.read()
-        # get bounding box coords and data
-        data['content'], bbox, _ = detector.detectAndDecode(img)
-
-        # if there is a bounding box, draw one, along with the data
-        if (bbox is not None):
-
-            cv2.putText(img, data['content'], (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (0, 255, 0), 2)
-            if data['content']:
-                mycursor = mydb.cursor()
-                mycursor.execute("select password from student where password = %s", [data['content']])
-                fetched_data = mycursor.fetchone()
-
-                if fetched_data is None:
-                    data['Found'] = "false"
-                    parsed_json = json.dumps(data)
-                    publish(myChannel, {"Data": "Invalid"})
-                    return str(parsed_json)
-                else:
-                    if fetched_data[0] == data['content']:
-                        print("data Valid")
-                        # LED setup
-                        GPIO.setmode(GPIO.BOARD)
-                        GPIO.setup(8, GPIO.OUT)
-
-                        # control LED when data received - set output to HIGH
-                        for i in range(0, 1):
-                            GPIO.output(8, True)
-                            time.sleep(0.5)
-                            GPIO.output(8, False)
-                            time.sleep(0.5)
-                        trigger = True
-                        publish(myChannel, {"Data": "Valid"})
-
-                        GPIO.cleanup()
-                        mycursor.close()
-                        data['Found'] = "true"
-                        parsed_json = json.dumps(data)
-                        return str(parsed_json)
-
-        # display the image preview
-        cv2.imshow("code detector", img)
-        if (cv2.waitKey(1) == ord("q")):
-            break
-
-    # free camera object and exit
-    cap.release()
-    cv2.destroyAllWindows()
 
 
 # pubnub code
@@ -157,7 +96,7 @@ def my_publish_callback(envelope, status):
 
 
 class MySubscribeCallback(SubscribeCallback):
-
+    
     def presence(self, pubnub, presence):
         pass  # handle incoming presence data
 
@@ -185,8 +124,11 @@ class MySubscribeCallback(SubscribeCallback):
             print(message.message)
             msg = message.message
             key = list(msg.keys())
+            print(key)
             if key[0] == "event":  # {"event":{"sensor_name":True}}
                 self.handleEvent(msg)
+            if key[0] == "scan":
+                self.qrgenerate()
         except Exception as e:
             print("Received: ", message.message)
             print(e)
@@ -202,19 +144,79 @@ class MySubscribeCallback(SubscribeCallback):
             if eventData[key[0]] is False:
                 dataD["alarm"] = False
 
+    def qrgenerate(self):
+        dataD["alarm"] = False
+        trigger = False
+        # set up camera object
+        cap = cv2.VideoCapture(-1)
+
+        # QR code detection object
+        detector = cv2.QRCodeDetector()
+        while True:
+            # get the image
+            _, img = cap.read()
+            # get bounding box coords and data
+            data['content'], bbox, _ = detector.detectAndDecode(img)
+
+            # if there is a bounding box, draw one, along with the data
+            if (bbox is not None):
+
+                cv2.putText(img, data['content'], (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5, (0, 255, 0), 2)
+                if data['content']:
+                    mycursor = mydb.cursor()
+                    mycursor.execute("select student_password from student where student_password = %s",
+                                     [data['content']])
+                    fetched_data = mycursor.fetchone()
+
+                    if fetched_data is None:
+                        data['Found'] = "false"
+                        parsed_json = json.dumps(data)
+                        publish(myChannel, {"Data": "Invalid"})
+                        return str(parsed_json)
+                    else:
+                        if fetched_data[0] == data['content']:
+                            print("data Valid")
+                            # LED setup
+                            GPIO.setmode(GPIO.BOARD)
+                            GPIO.setup(8, GPIO.OUT)
+
+                            # control LED when data received - set output to HIGH
+                            for i in range(0, 1):
+                                GPIO.output(8, True)
+                                time.sleep(0.5)
+                                GPIO.output(8, False)
+                                time.sleep(0.5)
+                            trigger = True
+                            publish(myChannel, {"data": "valid"})
+
+                            GPIO.cleanup()
+                            mycursor.close()
+                            data['Found'] = "true"
+                            parsed_json = json.dumps(data)
+                            cv2.imshow("code detector", img)
+                            return str(parsed_json)
+
+            # display the image preview
+            cv2.imshow("code detector", img)
+            if (cv2.waitKey(1) == ord("q")):
+                break
+
+        # free camera object and exit
+        cap.release()
+        cv2.destroyAllWindows()
+
 
 pubnub.add_listener(MySubscribeCallback())
 pubnub.subscribe().channels(myChannel).execute()
 
+
 if __name__ == '__main__':
-    app.run(host='192.168.43.136', port=9000)
+    app.run(host='192.168.43.136', port=8080)
     pubnub.add_listener(MySubscribeCallback())
     pubnub.subscribe().channels(myChannel).execute()
 
     # server = Server(app.wsgi_app)
     # server.serve()
-
-
-
-
 
