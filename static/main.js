60257b57af7fbf17f674ac29de0b38249ea57567
SiyaSalekar <92158161+SiyaSalekar@users.noreply.github.com>
2022-11-23 08:35:40 +0000

    qr code Working complete :tada:

--- static/main.js
+++ static/main.js
@@ -5,60 +5,58 @@
 ========== Start Siya's Code for QR Scan==============
 */
 
-
-
-function qrcodeFunc(){
-    fetch("/qrgenerate")
-      .then(response=>{
-		if(response.ok){
-			return response.json();
-		}
-		throw new Error("Server offline")
-	})
-	.then(responseJson => {
-		if(responseJson.Found == "true"){
-		    console.log("calling publish")
-		    publishMessage("Valid");
-			document.getElementById("data_id").innerHTML = "Valid";
-		}
-		else
-		{
-		    publishMessage("Invalid");
-			document.getElementById("data_id").innerHTML = "Invalid";
-		}
-
-		console.log(responseJson)})
-	.catch(error => console.log(error));
-}
+const siyaScanButton = document.getElementById('siya-scan-btn');
 
 //Pubnub - siyas-channel
-let myChannel = "siyas-channel"
-let pubnub;
+const myChannel = "siyas-channel";
 
-const setupPubNub = ()=>{
-	pubnub = new PubNub({
+const setupPubNub = () => {
+	const pubnub = new PubNub({
 		publishKey:
 			"pub-c-7b7bf3ef-a5df-4ec8-9bb2-f70cc90f6c86",
 		subscribeKey:
 			"sub-c-babca055-8ae8-4cbb-87e3-d1927bc7826a",
-		userId:"siyas-machine"
+		userId:"Jack-device"
 	});
-	const listener ={
+	pubnub.addListener({
 		status:(statusEvent)=>{
 			if(statusEvent.category === "PNConnectedCategory"){
-				console.log("Connected to PubNub")
+				console.log("Connected to PubNub");
+				siyaScanButton.removeAttribute('disabled');
 			}
 		},
 		message:(messageEvent)=>{
-			showMessage(messageEvent.message.description);
+			console.log(messageEvent);
+			if(messageEvent.message.hasOwnProperty('data')){
+			    if('valid' == messageEvent.message['data']){
+			        document.getElementById("data_id").innerHTML = "Valid";
+			    } else {
+			        document.getElementById("data_id").innerHTML = "Invalid";
+			    }
+
+			}
+
 		},
 		presence:(presenceEvent)=>{
 
 		}
-	}
-	pubnub.addListener(listener);
+	});
 	//subscribe to a channel
 	pubnub.subscribe({channels:[myChannel]});
+
+	siyaScanButton.addEventListener("click", async () => {
+        try{
+            const result = await pubnub.publish({
+                message: {
+                    scan: 'qrcode'
+                },
+                channel: myChannel,
+            });
+        } catch(err) {
+        console.error(err);
+        }
+
+    });
 };
 
 const publishMessage = async(message) =>{
@@ -71,7 +69,6 @@ const publishMessage = async(message) =>{
 	};
 	await pubnub.publish(publishPayload);
 }
-//endRegion Pubnub
 
 /*
 ========== ENDING Siya's Code ==============
