18fd7f7ef3f98c2c1cbe6f623f17fa7b14dd4501
SiyaSalekar <92158161+SiyaSalekar@users.noreply.github.com>
2022-12-01 17:53:13 +0000

    added Database code mydb.py code (on AWS) :sparkles:

---
+++ mydb.py
@@ -0,0 +1,75 @@
+# Database code - on AWS
+
+from flask_sqlalchemy import SQLAlchemy
+from .__init__ import db
+
+
+class UserTable(db.Model):
+    __tablename__ = "student"
+    student_id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(30))
+    student_number = db.Column(db.String(30))
+    auth_key = db.Column(db.String(255))
+    login = db.Column(db.Integer)
+
+    # constructor
+    def __init__(self, email, student_id, auth_key, login, student_number):
+        self.email = email
+        self.student_id = student_id
+        self.auth_key = auth_key
+        self.student_number = student_number
+        self.login = login
+
+
+def delete_all():
+    try:
+        db.session.query(UserTable).delete()
+        db.session.commit()
+        print("delete all")
+    except Exception as e:
+        print("Failed "+str(e))
+        db.session.rollback()
+
+
+def get_user_row_if_exists(student_id):
+    get_user_row = UserTable.query.filter_by(student_id=student_id).first()
+    if get_user_row is not None:
+        return get_user_row
+    else:
+        print("Student doesnt exist")
+        return False
+
+
+def add_user_and_login(email, student_id):
+    row = get_user_row_if_exists(student_id)
+    if row is not False:
+        row.login = 1
+        db.session.commit()
+    else:
+        print("Adding Student "+email)
+        new_user = UserTable(email, student_id, student_number, None, 1)
+        db.session.add(new_user)
+        db.session.commit()
+
+
+def user_logout(student_id):
+    row = get_user_row_if_exists(student_id)
+    if row is not False:
+        row.login = 0
+        db.session.commit()
+        print("Student "+row.email+" logged out")
+
+
+def add_auth_key(student_id, auth_key):
+    row = get_user_row_if_exists(student_id)
+    if row is not False:
+        row.authkey = auth_key
+        db.session.commit()
+        print("Student "+row.name+" authkey added")
+
+
+def view_all():
+    row = UserTable.query.all()
+    for n in range(0, len(row)):
+        print(str(row[n].student_id) + " | "+ row[n].email +" | "+ str(row[n].student_number) + " | " + str(row[n].authkey) + " | " + str(row[n].login))
+
